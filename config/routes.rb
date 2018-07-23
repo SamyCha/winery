a2169dc687c0e6a70f786477ab93cb14fb54c5f7
Samy Chalakani <samychalakani@gmail.com>
2018-07-23 17:00:56 +0200

    room crud

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,16 @@ Rails.application.routes.draw do
              path_names: {sign_in: 'login', sign_out: 'logout', edit: 'profile', sign_up: 'registration'},
              controllers: {omniauth_callbacks: 'omniauth_callbacks', registrations: 'registrations'}
 
-
- resources :users, only: [:show]
+  resources :users, only: [:show]
+  resources :rooms, except: [:edit] do
+    member do
+      get 'listing'
+      get 'pricing'
+      get 'description'
+      get 'photo_upload'
+      get 'amenities'
+      get 'location'
+    end
+  end
 
 end
