6779c3b76537b278f745d99975e4ef43e5643654
Samy Chalakani <samychalakani@gmail.com>
2018-07-26 15:32:22 +0200

    omniauth facebook production

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,11 @@ module ApplicationHelper
   def avatar_url(user)
     if user.image
       "http://graph.facebook.com/#{user.uid}/picture?type=large"
-    else
-      gravatar_id = Digest::MD5::hexdigest(user.email).downcase
-      "https://www.gravatar.com/avatar/#{gravatar_id}.jpg?d=identical&s=150"
-    end
-  end
+    else #default avatar if the user desn't have one
+     gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+     "http://gravatar.com/avatar/#{gravatar_id}.png"   end
+   end
+
+
+
 end
